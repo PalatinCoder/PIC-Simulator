b1c0d8de25b9341a4fa956cc64958dcf3ab8e35c
PalatinCoder <github@jan-sl.de>
2017-04-03 23:46:19 +0100

    TASK: Implement ProcessorInstruction data model

---
+++ Model/ProcessorInstruction.cs
@@ -0,0 +1,18 @@
+﻿namespace PIC_Simulator
+{
+    /// <summary>
+    /// Basisklasse um eine Instruction für den µC zu beschreiben
+    /// </summary>
+    public class ProcessorInstruction
+    {
+        public string Mnemonic { get; }
+        public int[] Operands { get; } = new int[2];
+
+        public ProcessorInstruction(string mnemonic, int operand0 = 0, int operand1 = 0)
+        {
+            this.Mnemonic = mnemonic;
+            this.Operands[0] = operand0;
+            this.Operands[1] = operand1;
+        }
+    }
+}
