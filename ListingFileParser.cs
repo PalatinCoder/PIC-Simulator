99fbfc7eb9c77d7122ca102d7c7e93809cd56e55
PalatinCoder <github@jan-sl.de>
2017-04-05 17:00:11 +0200

    TASK: Dummy parser
    returns null for every line for now :see_no_evil:

---
+++ ListingFileParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using System.Collections.ObjectModel;
+
+namespace PIC_Simulator
+{
+    /// <summary>
+    /// Parst die Textfiles und setzt sie in die jeweiligen Instructions um
+    /// </summary>
+    class ListingFileParser
+    {
+        /// <summary>
+        /// Parst das angegebene Textfile
+        /// </summary>
+        /// <param name="file">fileobjekt</param>
+        /// <param name="lineParsedCallback">Callback when a line was parsed to an instruction</param>
+        /// <returns>Collection von Instructions</returns>
+        public static async Task Parse(StorageFile file, Action<Instruction> lineParsedCallback)
+        {
+            IList<String> listing = await FileIO.ReadLinesAsync(file);
+            Collection<Instruction> programm = new Collection<Instruction>();
+
+            foreach (String line in listing)
+            {
+                Instruction instruction = ParseLine(line);
+                if (instruction != null) lineParsedCallback(instruction);
+            }
+        }
+
+        /// <summary>
+        /// Wandelt eine Zeile aus dem Listing in eine Instruction um
+        /// </summary>
+        /// <param name="line">Quellcodezeile</param>
+        /// <returns>Instruction|null</returns>
+        private static Instruction ParseLine(String line)
+        {
+            /*
+             * Index,Länge   Inhalt
+             * 0,4           LfdNr Zeile mit Ops
+             * 5,4           Op als Byte (hex)
+             * 20,5          LfdNr
+             * 27,9 oder *   Label
+             * 36,*          Payload
+             */
+
+
+            return null;
+        }
+    }
+}
