8f5bf7ac4b4757e64a241c5d6f9ab765899d79c3
PalatinCoder <github@jan-sl.de>
2017-05-04 14:16:07 +0200

    TASK: Implement value converters
    This adds value converters to be used in the view.

---
+++ Utility/FormatBinaryConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PIC_Simulator
+{
+    public class FormatBinaryConverter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        // This converts the value object to the string to display.
+        // This will work with most simple types.
+        public object Convert(object value, Type targetType,
+            object parameter, string language)
+        {
+            // Retrieve the format string and use it to format the value.
+            return System.Convert.ToString((byte)value, 2).PadLeft(8, '0');
+        }
+
+        // No need to implement converting back on a one-way binding
+        public object ConvertBack(object value, Type targetType,
+            object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
