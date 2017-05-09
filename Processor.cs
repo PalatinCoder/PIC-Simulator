03ca54e9fad27927111dc38ad9b119c209116700
PalatinCoder <github@jan-sl.de>
2017-05-09 16:35:51 +0200

    FIX: Use PC property
    holy shit :see_no_evil:

--- Processor.cs
+++ Processor.cs
@@ -64,7 +64,7 @@ namespace PIC_Simulator
         internal void Reset()
         {
             this.memController.clearMemory();
-            this.memController.SetPC(0);
+            this.memController.PC = 0;
             this.Wreg = 0;
             ViewInterface.SetCurrentSourcecodeLine(this.ProgramMemory[0].LineNumber - 1);
         }
